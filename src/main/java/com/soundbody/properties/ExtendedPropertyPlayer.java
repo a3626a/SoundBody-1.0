4c76bb99c939f7725264fcc97cab38b6a6715f4f
a3626a <a3626a@naver.com>
2015-08-13 08:40:23 +0900

    ServerSide에서만 계산

--- src/main/java/com/soundbody/properties/ExtendedPropertyPlayer.java
+++ src/main/java/com/soundbody/properties/ExtendedPropertyPlayer.java
@@ -41,41 +41,44 @@ public class ExtendedPropertyPlayer implements IExtendedEntityProperties {
 	}
 
 	public void update() {
-		lastFitness = fitness;
 
-		FoodStats foodStats = player.getFoodStats();
-		if (foodStats instanceof ModFoodStats) {
-			ModFoodStats modFoodStats = (ModFoodStats) foodStats;
-			double hungerPercent = foodStats.getFoodLevel() / (double) modFoodStats.getMaxFoodLevel();
-			if (hungerPercent > 0.75) {
-				if (fitnessCounter == 0) {
-					foodStats.addExhaustion(4.0F);
-					fitness += 1;
+		if (!player.worldObj.isRemote) {
+			lastFitness = fitness;
+
+			FoodStats foodStats = player.getFoodStats();
+			if (foodStats instanceof ModFoodStats) {
+				ModFoodStats modFoodStats = (ModFoodStats) foodStats;
+				double hungerPercent = foodStats.getFoodLevel() / (double) modFoodStats.getMaxFoodLevel();
+				if (hungerPercent > 0.75) {
+					if (fitnessCounter == 0) {
+						foodStats.addExhaustion(4.0F);
+						fitness += 1;
+						fitnessCounter = 60 * 20;
+					}
+				}
+				if (hungerPercent < 0.50) {
+					foodStats.setFoodSaturationLevel(foodStats.getSaturationLevel() + 1.0F);
+					fitness -= 1;
 					fitnessCounter = 60 * 20;
 				}
 			}
-			if (hungerPercent < 0.50) {
-				foodStats.setFoodSaturationLevel(foodStats.getSaturationLevel() + 1.0F);
-				fitness -= 1;
-				fitnessCounter = 60 * 20;
-			}
-		}
-		if (fitnessCounter > 0)
-			fitnessCounter--;
+			if (fitnessCounter > 0)
+				fitnessCounter--;
 
-		if (!player.worldObj.isRemote && lastFitness != fitness) {
-			PacketGeneralClient msg = new PacketGeneralClient(0);
-			msg.setInt(fitness);
-			SoundBody.simpleChannel.sendTo(msg, MinecraftServer.getServer().getConfigurationManager().getPlayerByUUID(player.getUniqueID()));
+			if (lastFitness != fitness) {
+				PacketGeneralClient msg = new PacketGeneralClient(0);
+				msg.setInt(fitness);
+				SoundBody.simpleChannel.sendTo(msg, MinecraftServer.getServer().getConfigurationManager().getPlayerByUUID(player.getUniqueID()));
+			}
 		}
 	}
 
 	public int getFitness() {
 		return fitness;
 	}
-	
+
 	public void sync(PacketGeneralClient packet) {
-		fitness=packet.getInt();
+		fitness = packet.getInt();
 	}
 
 }
