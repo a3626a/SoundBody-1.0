48b8324172994884b32db14d46b0434f42f580cc
a3626a <a3626a@naver.com>
2015-08-13 12:49:15 +0900

    음식 레벨 GUI가 세 줄 이상이 되더라도 공깃방울과 안 겹치도로 수정
    제곧내

--- src/main/java/com/soundbody/client/RenderFoodBarHandler.java
+++ src/main/java/com/soundbody/client/RenderFoodBarHandler.java
@@ -16,93 +16,91 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class RenderFoodBarHandler extends Gui {
 
 	public static int left_height = 39;
-    public static int right_height = 39;
-	
+	public static int right_height = 39;
+
 	@SubscribeEvent
 	public void renderFoodBar(RenderGameOverlayEvent.Pre event) {
 		if (event.type == ElementType.FOOD) {
 			event.setCanceled(true);
 
 			right_height = 39;
-	        left_height = 39;
-			
+			left_height = 39;
+
 			Minecraft mc = Minecraft.getMinecraft();
 			int width = event.resolution.getScaledWidth();
 			int height = event.resolution.getScaledHeight();
-			
+
 			mc.mcProfiler.startSection("food");
 
-	        EntityPlayer player = (EntityPlayer)mc.getRenderViewEntity();
-	        GlStateManager.enableBlend();
-	        int left = width / 2 + 91;
-	        int top = height - right_height;
-	        right_height += 10;
-	        boolean unused = false;// Unused flag in vanilla, seems to be part of a 'fade out' mechanic
+			EntityPlayer player = (EntityPlayer) mc.getRenderViewEntity();
+			GlStateManager.enableBlend();
+			int left = width / 2 + 91;
+			int top = height - right_height;
+			right_height += 10;
+			boolean unused = false;// Unused flag in vanilla, seems to be part
+									// of a 'fade out' mechanic
 
-	        FoodStats stats = mc.thePlayer.getFoodStats();
-	        ModFoodStats modStats = null;
-	        if (stats instanceof ModFoodStats) {
-	        	modStats = (ModFoodStats)stats;
-	        }
-		
-	        int level = stats.getFoodLevel();
-	        int levelLast = stats.getPrevFoodLevel();
-	        
-	        int cell = 10;
-	        if (modStats != null) cell = (modStats.getMaxFoodLevel()+1)/2;
-	        
-	        if(cell <= 10)
-	        	GuiIngameForge.right_height += 10;
-	        else GuiIngameForge.right_height += 18;
-	        
-	        for (int i = 0; i < cell; ++i)
-	        {
-	            int idx = i * 2 + 1;
-	            int x = left - (i%10) * 8 - 9;
-	            int y = top - 8*(i/10);
-	            int icon = 16;
-	            byte backgound = 0;
+			FoodStats stats = mc.thePlayer.getFoodStats();
+			ModFoodStats modStats = null;
+			if (stats instanceof ModFoodStats) {
+				modStats = (ModFoodStats) stats;
+			}
 
-	            if (mc.thePlayer.isPotionActive(Potion.hunger))
-	            {
-	                icon += 36;
-	                backgound = 13;
-	            }
-	            if (unused) backgound = 1; //Probably should be a += 1 but vanilla never uses this
+			int level = stats.getFoodLevel();
+			int levelLast = stats.getPrevFoodLevel();
 
-	            /*
-	            if (player.getFoodStats().getSaturationLevel() <= 0.0F && updateCounter % (level * 3 + 1) == 0)
-	            {
-	                y = top + (rand.nextInt(3) - 1);
-	            }
-	            */
-	            // This code make 'jumping' icons. but it is very difficult to define updateCounter... should do
-	            
-	            
-	            drawTexturedModalRect(x, y, 16 + backgound * 9, 27, 9, 9);
+			int cell = 10;
+			if (modStats != null)
+				cell = (modStats.getMaxFoodLevel() + 1) / 2;
 
-	            if (unused)
-	            {
-	                if (idx < levelLast)
-	                    drawTexturedModalRect(x, y, icon + 54, 27, 9, 9);
-	                else if (idx == levelLast)
-	                    drawTexturedModalRect(x, y, icon + 63, 27, 9, 9);
-	            }
+			GuiIngameForge.right_height += 10 + 8 * ((cell - 1) / 10);
 
-	            if (idx < level)
-	                drawTexturedModalRect(x, y, icon + 36, 27, 9, 9);
-	            else if (idx == level)
-	                drawTexturedModalRect(x, y, icon + 45, 27, 9, 9);
-	        }
-	        GlStateManager.disableBlend();
-	        mc.mcProfiler.endSection();
+			for (int i = 0; i < cell; ++i) {
+				int idx = i * 2 + 1;
+				int x = left - (i % 10) * 8 - 9;
+				int y = top - 8 * (i / 10);
+				int icon = 16;
+				byte backgound = 0;
+
+				if (mc.thePlayer.isPotionActive(Potion.hunger)) {
+					icon += 36;
+					backgound = 13;
+				}
+				if (unused)
+					backgound = 1; // Probably should be a += 1 but vanilla
+									// never uses this
+
+				/*
+				 * if (player.getFoodStats().getSaturationLevel() <= 0.0F &&
+				 * updateCounter % (level * 3 + 1) == 0) { y = top +
+				 * (rand.nextInt(3) - 1); }
+				 */
+				// This code make 'jumping' icons. but it is very difficult to
+				// define updateCounter... should do
+
+				drawTexturedModalRect(x, y, 16 + backgound * 9, 27, 9, 9);
+
+				if (unused) {
+					if (idx < levelLast)
+						drawTexturedModalRect(x, y, icon + 54, 27, 9, 9);
+					else if (idx == levelLast)
+						drawTexturedModalRect(x, y, icon + 63, 27, 9, 9);
+				}
+
+				if (idx < level)
+					drawTexturedModalRect(x, y, icon + 36, 27, 9, 9);
+				else if (idx == level)
+					drawTexturedModalRect(x, y, icon + 45, 27, 9, 9);
+			}
+			GlStateManager.disableBlend();
+			mc.mcProfiler.endSection();
 		}
 	}
-	
+
 	@SubscribeEvent
 	public void renderFoodAttributeBar(RenderGameOverlayEvent.Post event) {
-		if(event.type == ElementType.ALL) {
-			
+		if (event.type == ElementType.ALL) {
+
 		}
 	}
 }
