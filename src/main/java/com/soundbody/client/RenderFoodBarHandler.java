e9f27c60cdf5676173d2fed5b7434f702108d53c
Abastro <abab9579@gmail.com>
2015-08-13 16:11:19 +0900

    Render Change

--- src/main/java/com/soundbody/client/RenderFoodBarHandler.java
+++ src/main/java/com/soundbody/client/RenderFoodBarHandler.java
@@ -1,10 +1,12 @@
 package com.soundbody.client;
 
-import com.soundbody.foodstats.ModFoodStats;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.FoodStats;
@@ -12,6 +14,9 @@ import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import com.soundbody.foodstats.ModFoodStats;
+import com.soundbody.modifiers.ModAttributes;
 
 public class RenderFoodBarHandler extends Gui {
 
@@ -100,7 +105,84 @@ public class RenderFoodBarHandler extends Gui {
 	@SubscribeEvent
 	public void renderFoodAttributeBar(RenderGameOverlayEvent.Post event) {
 		if (event.type == ElementType.ALL) {
-
+			Minecraft mc = Minecraft.getMinecraft();
+			EntityPlayer player = mc.thePlayer;
+			
+			this.drawAttributebarAt(player.getEntityAttribute(SharedMonsterAttributes.movementSpeed), mc, "Speed", 0, 0, 50, 8, 0xff8080ff);
+			this.drawAttributebarAt(player.getEntityAttribute(SharedMonsterAttributes.attackDamage), mc, "Damage", 0, 8, 50, 8, 0xffff8080);
+			this.drawAttributebarAt(player.getEntityAttribute(ModAttributes.digspeedFactor), mc, "DigSp", 0, 16, 50, 8, 0xff00ff00);
+			this.drawAttributebarAt(player.getEntityAttribute(ModAttributes.jumpFactor), mc, "Jump", 0, 24, 50, 8, 0xffdddddd);
+			
+			double centerX = 50.0;
+			double centerY = 50.0;
+			double radius = 30.0;
+			double angle = 0.0;
+			double x = 0.0, y = 0.0;
+			double preX = 0.0, preY = 0.0;
+			double rate;
+			boolean flag = false;
+			
+			for(EnumAttribute attribute : EnumAttribute.values()) {
+				rate = attribute.getRate(player);
+				
+				x = centerX + rate * radius * Math.cos(angle);
+				y = centerY + rate * radius * Math.sin(angle);
+								
+				angle += 2 * Math.PI / EnumAttribute.values().length;
+				
+				drawRect((int)(x-3), (int)(y-3), (int)(x+3), (int)(y+3), attribute.getColor());
+				
+				if(flag)
+					drawLine(preX, preY, x, y, 0xffffffff);
+								
+				preX = x;
+				preY = y;
+				
+				flag = true;
+			}
+			
+			EnumAttribute attribute = EnumAttribute.values()[0];
+			rate = attribute.getRate(player);
+			x = centerX + rate * radius * Math.cos(angle);
+			y = centerY + rate * radius * Math.sin(angle);
+			
+			drawLine(preX, preY, x, y, 0xffffffff);
 		}
+	}
+	
+	public void drawAttributebarAt(IAttributeInstance attribute, Minecraft mc, String text, int left, int top, int width, int height, int color) {
+		double rate = attribute.getAttributeValue() / attribute.getBaseValue();
+		this.drawString(mc.fontRendererObj, text, left, top, color);
+		left += 20;
+		drawRect(left, top, left + (int) (width * rate), top + height, color);
+		drawRect(left + width - 1, top, left + width + 1, top + height, 0x80000000);
+	}
+	
+	public void drawLine(double x1, double y1, double x2, double y2, int color) {
+        double xd = y1 - y2;
+        double yd = x2 - x1;
+        double dist = Math.sqrt(xd * xd + yd * yd);
+        xd /= dist;
+        yd /= dist;
+        
+        float f3 = (float)(color >> 24 & 255) / 255.0F;
+        float f = (float)(color >> 16 & 255) / 255.0F;
+        float f1 = (float)(color >> 8 & 255) / 255.0F;
+        float f2 = (float)(color & 255) / 255.0F;
+		
+		Tessellator tessellator = Tessellator.getInstance();
+        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+        GlStateManager.enableBlend();
+        GlStateManager.disableTexture2D();
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+        GlStateManager.color(f, f1, f2, f3);
+        worldrenderer.startDrawingQuads();
+        worldrenderer.addVertex(x1 + xd, y1 + yd, 0.0D);
+        worldrenderer.addVertex(x2 + xd, y2 + yd, 0.0D);
+        worldrenderer.addVertex(x2 - xd, y2 - yd, 0.0D);
+        worldrenderer.addVertex(x1 - xd, y1 - yd, 0.0D);
+        tessellator.draw();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
 	}
 }
