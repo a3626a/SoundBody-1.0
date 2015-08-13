a790ddf329daaebaf8cf55eb8852e8ac9d41042e
a3626a <a3626a@naver.com>
2015-08-13 13:22:23 +0900

    Fix Bug
    Attribute Modifier의 갱신

--- src/main/java/com/soundbody/properties/ExtendedPropertyPlayer.java
+++ src/main/java/com/soundbody/properties/ExtendedPropertyPlayer.java
@@ -1,17 +1,22 @@
 package com.soundbody.properties;
 
-import com.soundbody.SoundBody;
-import com.soundbody.foodstats.ModFoodStats;
-import com.soundbody.network.PacketGeneralClient;
+import java.util.UUID;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.FoodStats;
 import net.minecraft.world.World;
 import net.minecraftforge.common.IExtendedEntityProperties;
+
+import com.soundbody.SoundBody;
+import com.soundbody.foodstats.ModFoodStats;
+import com.soundbody.lib.Strings;
+import com.soundbody.modifiers.ModAttributes;
+import com.soundbody.network.PacketGeneralClient;
 
 public class ExtendedPropertyPlayer implements IExtendedEntityProperties {
 
@@ -20,6 +25,8 @@ public class ExtendedPropertyPlayer implements IExtendedEntityProperties {
 	private int lastFitness;
 
 	private int fitnessCounter;
+
+	private static double factor = Math.log(1.5) / 20.0;
 
 	public ExtendedPropertyPlayer(EntityPlayer player) {
 		this.player = player;
@@ -54,14 +61,14 @@ public class ExtendedPropertyPlayer implements IExtendedEntityProperties {
 					if (fitnessCounter == 0) {
 						foodStats.addExhaustion(4.0F);
 						fitness += 1;
-						fitnessCounter = 10 * 20;
+						fitnessCounter = 20;
 					}
 				}
 				if (hungerPercent < 0.50) {
 					if (fitnessCounter == 0) {
 						foodStats.setFoodSaturationLevel(foodStats.getSaturationLevel() + 1.0F);
 						fitness -= 1;
-						fitnessCounter = 10 * 20;
+						fitnessCounter = 20;
 					}
 				}
 			}
@@ -72,8 +79,34 @@ public class ExtendedPropertyPlayer implements IExtendedEntityProperties {
 				PacketGeneralClient msg = new PacketGeneralClient(0);
 				msg.setInt(fitness);
 				SoundBody.simpleChannel.sendTo(msg, MinecraftServer.getServer().getConfigurationManager().getPlayerByUUID(player.getUniqueID()));
+				
+				player.getEntityAttribute(SharedMonsterAttributes.movementSpeed).removeModifier(new AttributeModifier(UUID.fromString(Strings.modifier_movementSpeed_uuid), Strings.modifier_movespeed_name, getAmount(1), 1));
+				player.getEntityAttribute(SharedMonsterAttributes.movementSpeed).applyModifier(new AttributeModifier(UUID.fromString(Strings.modifier_movementSpeed_uuid), Strings.modifier_movespeed_name, getAmount(1), 1));
+
+				player.getEntityAttribute(SharedMonsterAttributes.maxHealth).removeModifier(new AttributeModifier(UUID.fromString(Strings.modifier_maxHealth_uuid), Strings.modifier_maxHealth_name, getAmount(1), 1));
+				player.getEntityAttribute(SharedMonsterAttributes.maxHealth).applyModifier(new AttributeModifier(UUID.fromString(Strings.modifier_maxHealth_uuid), Strings.modifier_maxHealth_name, getAmount(1), 1));
+				
+				player.getEntityAttribute(SharedMonsterAttributes.attackDamage).removeModifier(new AttributeModifier(UUID.fromString(Strings.modifier_attackdamage_uuid), Strings.modifier_attackdamage_name, getAmount(1), 1));
+				player.getEntityAttribute(SharedMonsterAttributes.attackDamage).applyModifier(new AttributeModifier(UUID.fromString(Strings.modifier_attackdamage_uuid), Strings.modifier_attackdamage_name, getAmount(1), 1));
+				
+				player.getEntityAttribute(ModAttributes.digspeedFactor).removeModifier(new AttributeModifier(UUID.fromString(Strings.modifier_digspeedFactor_uuid), Strings.modifier_digspeed_name, getAmount(0), 0));
+				player.getEntityAttribute(ModAttributes.digspeedFactor).applyModifier(new AttributeModifier(UUID.fromString(Strings.modifier_digspeedFactor_uuid), Strings.modifier_digspeed_name, getAmount(0), 0));
+				
+				player.getEntityAttribute(ModAttributes.jumpFactor).removeModifier(new AttributeModifier(UUID.fromString(Strings.modifier_jumpFactor_uuid), Strings.modifier_jump_name, getAmount(0), 0));
+				player.getEntityAttribute(ModAttributes.jumpFactor).applyModifier(new AttributeModifier(UUID.fromString(Strings.modifier_jumpFactor_uuid), Strings.modifier_jump_name, getAmount(0), 0));
+
 			}
 		}
+	}
+
+	public double getAmount(int operation) {
+		switch (operation) {
+		case 0:
+			return getFitness() / 100;
+		case 1:
+			return Math.exp(factor * getFitness()) - 1;
+		}
+		return 0;
 	}
 
 	public int getFitness() {
